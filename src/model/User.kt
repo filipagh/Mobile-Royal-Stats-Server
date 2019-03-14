2a99e01007180970a43fd36f0c451046e8b15ce2
Martin Civáň <martin.civan5@gmail.com>
2019-03-14 23:03:23 +0100

    validacie a mavencok

--- src/model/User.kt
+++ src/model/User.kt
@@ -2,6 +2,9 @@ package model
 
 import java.io.Serializable
 import javax.persistence.*
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name="users")
@@ -12,7 +15,14 @@ class User : Serializable {
     @SequenceGenerator(name = "users_id", sequenceName = "users_id_seq", allocationSize = 1)
     var id: Int? = null
     var name: String? = null
+
+    @Email
+    @NotEmpty
+    @NotNull
     var email: String? = null
+
+    @NotNull(message = "Name cannot be null")
+    @NotEmpty(message = "Name cannot be null")
     var password: String? = null
     var salt: String? = null
     var apiKey: String? = null
