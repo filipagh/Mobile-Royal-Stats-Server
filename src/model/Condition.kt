e9647c819cf649d941ed8254c39d174033a573fb
Martin Civáň <martin.civan5@gmail.com>
2019-03-15 00:38:43 +0100

    a ty dalse veci (nake)

--- src/model/Condition.kt
+++ src/model/Condition.kt
@@ -2,32 +2,25 @@ package model
 
 import java.io.Serializable
 import javax.persistence.*
-import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 @Entity
-@Table(name="users")
-class User : Serializable {
+@Table(name="conditions")
+class Condition : Serializable {
 
     @Id
     @javax.persistence.GeneratedValue(strategy = GenerationType.AUTO, generator = "users_id")
     @SequenceGenerator(name = "users_id", sequenceName = "users_id_seq", allocationSize = 1)
     var id: Int? = null
     var name: String? = null
-    var gameId: String? = null
-    var role: String? = null
-
-//    @UniqueConstraint(name="email")
-    @Email
     @NotEmpty
     @NotNull
-    lateinit var email: String
-
-    @NotNull(message = "Name cannot be null")
-    @NotEmpty(message = "Name cannot be null")
-    var password: String? = null
-    var salt: String? = null
-    var apiKey: String? = null
-
+    var field: String? = null
+    @NotEmpty
+    @NotNull
+    var operator: String? = null
+    var value: String? = null
+    @ManyToOne
+    var clan : Clan? = null
 }
